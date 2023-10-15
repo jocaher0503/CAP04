1c5cb20447122426356417da7901b0c9e3df2954
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-15 14:21:57 +0200

    15/10/2023

--- CAP04/src/CAP04EJ01.java
+++ CAP04/src/CAP04EJ01.java
@@ -1,5 +1,27 @@
 public class CAP04EJ01 {
     public static void main(String[] args) throws Exception {
-        
+    String dia;
+    System.out.println("Por favor, introduzca un día de la semana y le diré qué asignatura toca a primera hora ese día: ");
+    dia = System.console().readLine(); 
+    dia = dia.toLowerCase();
+    switch(dia) {
+    case "lunes":     
+    case "jueves":
+        System.out.println("Programación");
+    break;
+    case "martes":
+    case "miércoles":       
+        System.out.println("Lenguaje de Marcas");
+    break;
+    case "viernes":
+        System.out.println("FOL");
+    break;
+    case "sábado":
+    case "domingo":
+        System.out.println("¡Ese día no hay clase!");
+    break;
+    default:
+        System.out.println("El día introducido no es correcto.");
+        }
     }
 }
---
+++ CAP04/src/CAP04EJ06.java
@@ -0,0 +1,5 @@
+public class CAP04EJ06 {
+    public static void main(String[] args) {
+        
+    }
+}
