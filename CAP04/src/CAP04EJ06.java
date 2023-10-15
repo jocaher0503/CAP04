7786330bda37ee0d63233dfd222cedb943a814f7
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-15 22:18:43 +0200

    15/10/2023

--- CAP04/src/CAP04EJ06.java
+++ CAP04/src/CAP04EJ06.java
@@ -1,5 +1,10 @@
 public class CAP04EJ06 {
     public static void main(String[] args) {
-        
-    }
+    System.out.println("Cálculo del tiempo de caída de un objeto.");
+    System.out.print("Por favor, introduzca la altura (en metros) desde la que cae el objeto: ");
+    Double h = Double.parseDouble(System.console().readLine());
+    final double g = 9.81;
+    Double t = Math.sqrt(2*h/g);
+    System.out.printf("El objeto tarda %.2f segundos en caer.\n", t);
+  }
 }
---
+++ CAP04/src/CAP04EJ07.java
@@ -0,0 +1,13 @@
+public class CAP04EJ07 {
+    public static void main(String[] args) {
+    System.out.println("Este programa calcula la media de tres notas.");
+    System.out.print("Por favor, introduzca la primera nota: ");
+    Double nota1 = Double.parseDouble(System.console().readLine());
+    System.out.print("Ahora introduzca la segunda nota: ");
+    Double nota2 = Double.parseDouble(System.console().readLine());
+    System.out.print("Por último introduzca la tercera nota: ");
+    Double nota3 = Double.parseDouble(System.console().readLine());
+    Double media = (nota1 + nota2 + nota3) / 3;
+    System.out.printf("La media es %.2f", media);    
+    }
+}
