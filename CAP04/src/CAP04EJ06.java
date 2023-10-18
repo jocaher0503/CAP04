fa6dc1d8b7421fb653e9ad881b947a0be159bb4b
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-18 12:25:56 +0200

    18/10/2023

--- CAP04/src/CAP04EJ06.java
+++ CAP04/src/CAP04EJ06.java
@@ -3,7 +3,7 @@ public class CAP04EJ06 {
     System.out.println("Cálculo del tiempo de caída de un objeto.");
     System.out.print("Por favor, introduzca la altura (en metros) desde la que cae el objeto: ");
     Double h = Double.parseDouble(System.console().readLine());
-    final double g = 9.81;
+    Double g = 9.81;
     Double t = Math.sqrt(2*h/g);
     System.out.printf("El objeto tarda %.2f segundos en caer.\n", t);
   }
